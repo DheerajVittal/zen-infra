4e30c07e5aefc58dea6177aeb9e03faf4222c639
Ranjini Ganeshan <32661402+ranjiniganeshan@users.noreply.github.com>
2026-06-25 10:29:40 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -20,7 +20,7 @@ module "eks" {
   env                = "dev"
   cluster_version    = "1.33"
   subnet_ids         = module.vpc.private_eks_subnet_ids
-  node_instance_type = "t3.large"
+  node_instance_type = "t3.small"
   desired_capacity   = 4
   min_size           = 1
   max_size           = 4
